3d9ea18dd80f460c484b945887334700c048c9fe
HunterAP <hunterap@gmail.com>
2023-02-16 22:22:30 -0500

    Added 7 Days to Die Module
    - Added AMP/Discord chat code
    - Added Avatar
    - Added 3 banners

---
+++ modules/SevenDaystoDie/cog_sevendays.py
@@ -0,0 +1,53 @@
+'''
+   Copyright (C) 2021-2022 Katelynn Cadwallader.
+
+   This file is part of Gatekeeper, the AMP Minecraft Discord Bot.
+
+   Gatekeeper is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   Gatekeeper is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Gatekeeper; see the file COPYING.  If not, write to the Free
+   Software Foundation, 51 Franklin Street - Fifth Floor, Boston, MA
+   02110-1301, USA. 
+
+'''
+from discord.ext import commands
+import os
+import logging
+
+import utils
+import AMP_Handler
+import DB
+
+DisplayImageSources = ['steam:251570']
+class Sevendays(commands.Cog):
+    def __init__ (self,client):
+        self._client = client
+        self.name = os.path.basename(__file__)
+        self.logger = logging.getLogger(__name__) #Point all print/logging statments here!
+
+        self.AMPHandler = AMP_Handler.getAMPHandler()
+        self.AMP = self.AMPHandler.AMP #Main AMP object
+        self.AMPInstances = self.AMPHandler.AMP_Instances #Main AMP Instance Dictionary
+
+        self.DBHandler = DB.getDBHandler()
+        self.DB = self.DBHandler.DB #Main Database object
+        self.DBCOnfig = self.DB.DBConfig
+
+        self.uBot = utils.botUtils(client)
+        self.dBot = utils.discordBot(client)
+        #self.uBot.sub_command_handler('user',self.info) #This is used to add a sub command(self,parent_command,sub_command)
+        self.logger.info(f'**SUCCESS** Initializing Module **{self.name.capitalize()}**')
+
+
+
+async def setup(client):
+    await client.add_cog(Sevendays(client))
